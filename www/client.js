837bacadced2fe071c8d33904e994f0e1701d659
Stefanie <54906263+stefrie@users.noreply.github.com>
2021-11-04 11:06:11 -0700

    Update client.js

--- www/client.js
+++ www/client.js
@@ -12,7 +12,7 @@ function updateNameValue(e) {
 }
 
 // URL to our Token Server
-const tokenServerURL = 'https://cascadiajs-dolbyapp.netlify.app/';
+const tokenServerURL = 'https://cascadiajs-dolbyapp.netlify.app//api/token-generator';
 
 
 
